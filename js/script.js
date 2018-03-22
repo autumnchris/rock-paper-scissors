56a241ebdf41932d7078cb455d9a97e5d9828b81
Autumn <autumncbullard@gmail.com>
2018-03-22 11:09:22 -0400

    Build function that will start the game

--- js/script.js
+++ js/script.js
@@ -4,3 +4,20 @@ result,
 playerScore,
 computerScore,
 endGameMsg;
+
+// FUNCTIONS
+
+function startGame() {
+  document.getElementById('modal').style.display = 'none';
+
+  round = 'Round 1: Select Rock, Paper, or Scissors.';
+  roundNum = 1;
+  result = '';
+  playerScore = 0;
+  computerScore = 0;
+
+  document.getElementById('round').innerHTML = round;
+  document.getElementById('result').innerHTML = result;
+  document.getElementById('player-score').innerHTML = playerScore;
+  document.getElementById('computer-score').innerHTML = computerScore;
+}
